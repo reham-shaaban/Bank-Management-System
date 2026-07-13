b972acb0d10d51f5fa5f34b71bf11956e75dd2ec
Reham Shaaban <shaabanreham381@gmail.com>
2026-07-13 04:18:05 +0300

    feat(transactions): add transfer functionality and optimize cash operations
    - Implement 	ransfer() feature to allow secure funds transfer between accounts.
    - Refactor withdraw() and deposit() to utilize safe stream validation.
    - Add helper functions money(), currentDate(), and updateTrans() for code reusability.
    - Add self-transfer protection and handle transaction ID overflow logic.

--- financialOperation.h
+++ financialOperation.h
@@ -2,6 +2,10 @@
 #include "Structures.h"
 #include "bankOperation.h"
 
+long generateTransactionID(long accountNum ,const bankAccount& currentAccount)
+{
+	return accountNum * 100 + currentAccount.transactions.size() + 1;
+}
 string currentDate()
 {
 	time_t t = time(0);
@@ -14,31 +18,34 @@ string currentDate()
 double money()
 {
 	double amount;
-	cout << "amount : "; cin >> amount;
+	cout << "amount : ";
+	if (!(cin >> amount) || amount <= 0)
+	{
+		cin.clear(); cin.ignore(10000, '\n');
+		cout << "Error !! You shoud enter positive amount\n";
+		return -1;
+	}
 	return amount;
 }
-bool withdraw()
+transaction updateTrans(long tID, double amount ,const string& tType,const string& rAccountN ,const string& time)
+{
+	transaction transactionInfo;
+	transactionInfo.amount = amount;
+	transactionInfo.receiverAccountNumber = rAccountN;
+	transactionInfo.timestamp = time;
+	transactionInfo.transactionID = tID;
+	transactionInfo.type = tType;
+	return transactionInfo;
+}
+bool withdraw(int index , long accountNum)
 {
 	cout << "\t--- Withdraw Money ---\n";
-	long accountNum = promptForAccountNumber();
-	int index = findAccountIndex(accountNum);
-   // التاكد ان الحساب موجود اولا 
-	if (index == -1)  
-	{
-		cout << "Account not found !!\n";
-		return false;
-	}
 	// طلب ادخال المبلغ
 	auto& currentAccount = accounts.at(index);
 	double amount = money();
 	string todayDate = currentDate();
-
-	if (amount <= 0)
-	{
-		cout << "Error !! You shoud enter positive amount\n";
-		return false;
-	}
-
+	//----------------------------------
+	if (amount == -1)   return false;
 	//التاكد من صلاحية الحساب لسحب المبلغ
 	if (amount > currentAccount.balance)
 	{
@@ -58,17 +65,11 @@ bool withdraw()
 		currentAccount.dailyWithdrawn = 0.0;
 		currentAccount.lastWithdrawDate = todayDate;
 	}
-
 	currentAccount.balance -= amount;
 	currentAccount.dailyWithdrawn += amount;
-	
 	// تحديث سجل اليوزر
-	transaction transactionInfo;
-	transactionInfo.amount = amount;
-	transactionInfo.type = "Withdraw";
-	transactionInfo.transactionID = accountNum * 1000 + currentAccount.transactions.size() + 1;
-	transactionInfo.timestamp = currentAccount.lastWithdrawDate;
-	currentAccount.transactions.push_back(transactionInfo);
+	long tID = generateTransactionID(accountNum, currentAccount);
+	currentAccount.transactions.push_back(updateTrans(tID, amount, "Withdraw", "--", todayDate));
     // تحديث بيانات البنك 
 	BankInfo.totalWithdrawn += amount;
 	BankInfo.totalBalanceInBank -= amount;
@@ -78,36 +79,20 @@ bool withdraw()
 	cout << "Your new balance : " << currentAccount.balance << endl;
 	return true;
 }
-bool deposit()
+bool deposit(int index , long accountNum)
 {
 	cout << "\t--- Deposit Money ---\n ";
-	long accountNum = promptForAccountNumber();
-	int index = findAccountIndex(accountNum);
-	// التاكد ان الحساب موجود اولا 
-	if (index == -1)
-	{
-		cout << "Account not found !!\n";
-		return false;
-	}
+	bankAccount& currentAccount = accounts.at(index);
 	// طلب ادخال المبلغ
-	auto& currentAccount = accounts.at(index);
 	double amount = money();
 	string todayDate = currentDate();
-
-	if (amount <= 0)
-	{
-		cout << "Error !! You shoud enter positive amount\n";
-		return false;
-	}
-
+	//--------------------------------
+	if (amount == -1)  return false;
+	//--------------------------------
 	currentAccount.balance += amount;
 	// تحديث سجل اليوزر == نجاح العميلة
-	transaction transactionInfo;
-	transactionInfo.amount = amount;
-	transactionInfo.type = "Deposit";
-	transactionInfo.transactionID = accountNum * 1000 + currentAccount.transactions.size() + 1;
-	transactionInfo.timestamp = todayDate;
-	currentAccount.transactions.push_back(transactionInfo);
+	long tID = generateTransactionID(accountNum, currentAccount);
+	currentAccount.transactions.push_back(updateTrans(tID, amount, "Deposit", "--", todayDate));
 	// تحديث بيانات البنك 
 	BankInfo.totalDeposited += amount;
 	BankInfo.totalBalanceInBank += amount;
@@ -116,5 +101,66 @@ bool deposit()
 	cout << "Deposit successful!\n";
 	cout << "Your new balance : " << currentAccount.balance << endl;
 	return true;
+}
+bool transfer(int index , long accountNum)
+{
+	cout << "\t--- Transfer Money ---\n";
+	cout << "the receiver's details \n";
+	long accountNumForReciever = promptForAccountNumber();
+	if (accountNumForReciever == accountNum)
+	{
+		cout << "Transfer failed! You cannot transfer money to your own account.\n";
+		return false;
+	}
+	int indexReciever = findAccountIndex(accountNumForReciever);
+	// التاكد ان الحساب المستلم موجود اولا 
+	if (indexReciever == -1)
+	{
+		cout << "Account not found !!\n";
+		return false;
+	}
+	// طلب ادخال المبلغ
+	auto& senderAccount = accounts.at(index);
+	auto& RecieverAccount = accounts.at(indexReciever);
+	double amount = money();
+	//--------------------------------
+	if (amount == -1)  return false;
+	//--------------------------------
+	string todayDate = currentDate();
+	//التاكد من صلاحية الحساب لسحب المبلغ
+	if (amount > senderAccount.balance)
+	{
+		cout << "Transfer failed! Unsufficiant balance\n";
+		return false;
+	}
+	if (todayDate == senderAccount.lastWithdrawDate)
+	{
+		if ((amount + senderAccount.dailyWithdrawn) > senderAccount.dailyLimit)
+		{
+			cout << "Transfer failed! Daily limit exceeded\n";
+			return false;
+		}
+	}
+	else
+	{
+		senderAccount.dailyWithdrawn = 0.0;
+		senderAccount.lastWithdrawDate = todayDate;
+	}
 
+	senderAccount.balance -= amount;
+	senderAccount.dailyWithdrawn += amount;
+	RecieverAccount.balance += amount;
+	// تحديث سجل الراسل 
+	long tID = generateTransactionID(accountNum, senderAccount);
+	senderAccount.transactions.push_back(updateTrans(tID, amount, "Transfer to",to_string(accountNumForReciever),todayDate));
+	// تحديث سجل المستلم
+	long tR_ID = generateTransactionID(accountNumForReciever, RecieverAccount);
+	RecieverAccount.transactions.push_back(updateTrans(tR_ID, amount, "Transfer from", to_string(accountNum), todayDate));
+   //تحديث سجل البنك 
+	BankInfo.totalTransferred += amount;
+	BankInfo.total_Active_Transactions++;
+
+	cout << "Transfer successful!\n";
+	cout << "Your new balance : " << senderAccount.balance << endl;
+	return true;
 }
